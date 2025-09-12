d0cb10c5023fc591abfe78379cf5e926f1ae5928
vsluch <vsluchenko2006@gmail.com>
2025-09-12 21:40:52 +0700

    Update ComputerEquipment.cs

--- ComputerEquipment.cs
+++ ComputerEquipment.cs
@@ -8,10 +8,10 @@ namespace laba1
 {
     public class ComputerEquipment
     {
-        protected string Brand {  get; set; }
-        protected string CpuBrand { get; set; }
-        protected int Ram { get; set; }
-        protected int Price { get; set; }
+        protected string Brand {  get; set; }       // производитель
+        protected string CpuBrand { get; set; }     // производитель процессора
+        protected int Ram { get; set; }             // объем оперативной памяти
+        protected int Price { get; set; }           // цена
 
 
         public ComputerEquipment(string brand = "Неизвестный", string cpu_brand = "Неизвестный", int ram = 0, int price = 0)
