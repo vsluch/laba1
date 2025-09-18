d833e562ab14e29f667fca6f8b7f7a425425b139
vsluch <vsluchenko2006@gmail.com>
2025-09-18 22:11:07 +0700

    Create IPrintable.cs

---
+++ IPrintable.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+}
